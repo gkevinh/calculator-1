33d46a7a5d0ee75a53c0a42127d31d897c54b54c
Kevin Wong <krw87@yahoo.com>
2022-11-08 20:47:01 -0800

    completed all functions

--- arithmetic.py
+++ arithmetic.py
@@ -35,12 +35,15 @@ def power(num1, num2):
     """Raise num1 to the power of num2 and return the value."""
     return num1 ** num2
 
+
 def mod(num1, num2):
     """Return the remainder of num1 / num2."""
     return num1 & num2
 
+
 def add_mult(num1, num2, num3):
     return (num1 + num2) * num3
+
 
 def add_cubes(num1, num2):
     return ((num1 * num1 * num1) + (num2 * num2 * num2))
